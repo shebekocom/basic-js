ac70bcc3dff5b3c01c1af00b1724443341dfbdb8
Nikolay Shebeko <nikolay.shebeko@gmail.com>
2020-09-10 01:37:57 +0300

    feat: add solution carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,8 +1,19 @@
-const CustomError = require('../extensions/custom-error');
+const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
-return false;
+  if (
+    typeof sampleActivity == "string" &&
+    parseFloat(sampleActivity) > 0 &&
+    parseFloat(sampleActivity) < 15
+  ) {
+    let k = 0.693 / HALF_LIFE_PERIOD;
+    let log = Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity));
+    let t = Math.ceil(log / k);
+    return t;
+  } else {
+    return false;
+  }
 };
